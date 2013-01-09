d18a59be80749ef904c978921be4b57793175981
a.l.e <ale@ideale.ch>
2013-01-09 16:51:37 +0100

    it almost works...

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ htdocs-blog-github
 ==================
 
 A simple php script publishing articles stored in a github repository.
+
+- get the list of the articles from a github repository
+- locally cache the articles and check the list of the files for changes by the sha hash
+
+- metadata is written in yaml (compatibility with jekyll)
+  - files without metadata are published as soon as they are uploaded (with the date of first discovering)
+  - metatadata without date is not yet published
+  - metadata must start with "---" at the beginning of the file and end with a "---"
+  - if you want the fields to be shown correctly on github markdown preview, you have to indent the metadata fields by four spaces (preformatted text)
+  - the following fields are recognized:
+    - date ([y]y.[m]m.dddd[ hh:mm])
+    - author
+    - tags (tag[,tag[,...]])
+- add a switch to rebuild the whole cache from scratch
+- the update script can be protected by a weak password (just to avoid that bots consume your precious github hits)
+
+uses mardown extra and spyc
--- index.php
+++ index.php
@@ -3,6 +3,35 @@
 ini_set('display_errors', '1');
 error_reporting(E_ALL);
 
+define('BLOG_CONFIG_PATH', 'data/config.php');
+define('BLOG_CONTENT_PATH', 'data/content.json');
+define('BLOG_LIST_PATH', 'data/list.json');
+define('BLOG_CACHE_PATH', 'data/cache/');
+
+$error = array();
+if (!is_dir(BLOG_CACHE_PATH)) {
+    if (!@mkdir(BLOG_CACHE_PATH, 777)) {
+        $error[] = "the ".BLOG_CACHE_PATH." directory does not exist and could not be created";
+    }
+}
+foreach (array(BLOG_CONTENT_PATH, BLOG_LIST_PATH, BLOG_CONFIG_PATH) as $item) {
+    if (!is_file($item)) {
+        if (!@touch($item)) {
+            $error[] = "the ".$item." file does not exist and could not be created";
+        }
+    } else if (!is_writable($item)) {
+        $error[] = "the ".$item." file is not writable";
+    }
+}
+
+if (!empty($error)) {
+    foreach ($error as $item) {
+        echo("<p>".$item."</p>\n");
+    }
+    die();
+}
+
+
 // phpinfo();
 function debug($label, $value) {
     echo("<p>$label<br /><pre>".htmlentities(print_r($value, 1))."</pre></p>");
@@ -21,63 +50,111 @@ function get_content_from_github($url) {
     return $content;
 }
 
-$url = "https://raw.github.com/aoloe/htdocs-xox/master/test.html";
-if (false) {
-    // $url = "http://tagesanzeiger.ch";
-    $url = "https://api.github.com/users/aoloe"; // does not work
-    $infos = get_content_from_github($url);
-    debug('get_content_from_github', $infos);
-}
-if (false) {
-    // $url = "http://tagesanzeiger.ch";
-    $url = "https://api.github.com/users/aoloe"; // does not work
-    $infos = get_content_from_github($url);
-    debug('get_content_from_github', $infos);
-}
-if (false) {
-    $url = "https://api.github.com/users/aoloe/repos"; // list of repos
-    $repos = json_decode(get_content_from_github($url));
-    debug('get_content_from_github', $repos[2]);
-    $url = "https://api.github.com/users/aoloe/htdocs-xox/contents/";
-    debug('get_content_from_github', json_decode(get_content_from_github($url)));
-}
-if (false) {
-    $url = "https://api.github.com/users/aoloe?rate_limit"; // general infomrmation about the account (nothing really interesting for now)
-    $repos = json_decode(get_content_from_github($url));
-    debug('get_content_from_github', json_decode(get_content_from_github($url)));
-}
-if (false) {
-    $url= "https://api.github.com/repos/aoloe/htdocs-xox/git/trees/0f9429f9868ea4b0d3ee674bc8b6a6a83ab90ea0";
-    // GET /repos/:owner/:repo/git/trees/:sha
-    // $url = "https://api.github.com/users/aoloe/git/trees/";
-    $tree = json_decode(get_content_from_github($url));
-    debug('tree', $tree);
-}
-if (true) {
-    $url= "https://api.github.com/repos/aoloe/htdocs-xox/contents/"; // gets the list of files in the directory
-    // GET /repos/:owner/:repo/contents/:path
-    $contents = json_decode(get_content_from_github($url));
-    debug('contents', $contents);
-}
+$url = "https://api.github.com/rate_limit";
+$rate_limit = json_decode(get_content_from_github($url));
+// debug('rate_limit', $rate_limit);
 
+echo("<p>".$rate_limit->rate->remaining." hits remaining of ".$rate_limit->rate->limit.".</p>");
 
+$url= "https://api.github.com/repos/aoloe/htdocs-xox/contents/"; // gets the list of files in the directory
+$url= "https://api.github.com/repos/aoloe/htdocs-blog-xox/contents/text"; // gets the list of files in the directory
+// GET /repos/:owner/:repo/contents/:path
+if (false) { // @XXX: only for debugging purpose: if false don't ping each time github
+    $content_github = get_content_from_github($url);
+    // debug('content', $content);
+    file_put_contents("content_github.json", $content_github);
+} else {
+    $content_github = file_get_contents("content_github.json");
+}
+$content_github = json_decode($content_github);
+// debug('content_github', $content_github);
+$content = array();
+if (file_exists(BLOG_CONTENT_PATH)) {
+    $content = file_get_contents(BLOG_CONTENT_PATH);
+    $content = json_decode($content);
+}
+if (!is_array($content)) {
+    $content = array();
+}
+// debug('content', $content);
+include_once "spyc.php";
+include_once "markdown.php";
 
-/* gets the contents of a file if it exists, otherwise grabs and caches */
-function get_repo_json($file,$plugin) {
-  //vars
-  $current_time = time(); $expire_time = 24 * 60 * 60; $file_time = filemtime($file);
-  //decisions, decisions
-  if(file_exists($file) && ($current_time - $expire_time < $file_time)) {
-    //echo 'returning from cached file';
-    return json_decode(file_get_contents($file));
-  }
-  else {
-    $json = array();
-    $json['repo'] = json_decode(get_content_from_github('http://github.com/api/v2/json/repos/show/darkwing/'.$plugin),true);
-    $json['commit'] = json_decode(get_content_from_github('http://github.com/api/v2/json/commits/list/darkwing/'.$plugin.'/master'),true);
-    $json['readme'] = json_decode(get_content_from_github('http://github.com/api/v2/json/blob/show/darkwing/'.$plugin.'/'.$json['commit']['commits'][0]['parents'][0]['id'].'/Docs/'.$plugin.'.md'),true);
-    $json['js'] = json_decode(get_content_from_github('http://github.com/api/v2/json/blob/show/darkwing/'.$plugin.'/'.$json['commit']['commits'][0]['parents'][0]['id'].'/Source/'.$plugin.'.js'),true);
-    file_put_contents($file,json_encode($json));
-    return $json;
-  }
+if (is_array($content_github)) {
+
+    $changed = false;
+    foreach ($content_github as $item) {
+        debug('item', $item);
+        if ($item->type == 'file') {
+            if (!array_key_exists($item->path, $content)) {
+                $content[$item->name] = array (
+                    'path' => $item->path,
+                    'name' => $item->name,
+                    'html_name' => pathinfo($item->name, PATHINFO_FILENAME).'.html',
+                    'raw_url' => 'https://raw.github.com/aoloe/htdocs-blog-xox/master/'.$item->path,
+                    'sha' => '',
+
+                    'author' => '',
+                    'title' => '',
+                    'tags' => '',
+                );
+            }
+            $content_item = $content[$item->name];
+            if ($item->sha != $content_item['sha']) {
+                $changed = true;
+                debug('item', $item);
+                $file = get_content_from_github($content_item['raw_url']);
+                debug('file', $file);
+                $yaml_end = 0;
+                if ((substr($file, 0, 3) == '---') && ($yaml_end = strpos($file, '---', 4))) {
+                    debug('yaml_end', $yaml_end);
+                    $metadata = Spyc::YAMLLoadString(substr($file, 4, $yaml_end));
+                    debug('metadata', $metadata);
+                    $content_item['date'] = (array_key_exists('date', $metadata) ? $metadata['date'] : date('d.m.Y'));
+                    if (array_key_exists('author', $metadata)) {
+                        $content_item['author'] = $metadata['author'];
+                    }
+                    if (array_key_exists('title', $metadata)) {
+                        $content_item['title'] = $metadata['title'];
+                    }
+                    $yaml_end += 4; // remove the closing ---
+                }
+                if ($yaml_end > 0) {
+                    $file = substr($file, $yaml_end);
+                }
+                debug('file', $file);
+                $header = array();
+                if ($content_item['title'] != '') {
+                    $header[] = '<h2 class="blog">'.$content_item['title'].'</h2>';
+                }
+                $header[] = sprintf(
+                    '<p class="blog_author_date">%s%s</p>', 
+                    $content_item['author'] == '' ? '' : $content_item['author'].', ',
+                    $content_item['date']
+                );
+                if ($content_item['tags'] != '') {
+                    $header[] = '<p class="blog_tags">'.$content_item['tags'].'</p>';
+                }
+                $file = implode("\n", $header)."\n".Markdown($file);
+                file_put_contents(BLOG_CACHE_PATH.$item->name, $file);
+                $content_item['sha'] = $item->sha;
+                $content[$item->name] = $content_item;
+                debug('content_item', $content_item);
+            } // if sha != sha
+        } // if file
+    } // foreach content_github
+    if ($changed) {
+        $list = array();
+        foreach ($content as $key => $value) {
+            $list[BLOG_CACHE_PATH.$value['html_name']] = strtotime($value['date']);
+        }
+        asort($list);
+        debug('list', $list);
+    }
+} // if is_array
+
+if (is_writable(BLOG_CONTENT_PATH)) {
+    //file_put_contents(BLOG_CONTENT_PATH, json_encode($content));
+} else {
+    echo("<p>Could not store content.json</p>");
 }
